097124e5b676577801ace58c16b1baea481dabe9
Daniel Kolev <danibanani.js@gmail.com>
2021-10-03 23:51:32 +0300

    Add controls to the scene and pass floors

--- src/components/three/canvas.tsx
+++ src/components/three/canvas.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import Box from "./Box";
+import Box from "./box";
+import Floors from "../../utils/floors";
+import Controls from "./controls";
 
 const IndexPage: React.FC = () => (
   <Canvas>
+    <Controls floors={Floors} />
     <ambientLight />
     <pointLight position={[10, 10, 10]} />
     <Box position={[3, 0, 0]} />
