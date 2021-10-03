137d7c9f56c5046c392a86ba8d493a61f18a4b0d
Daniel Kolev <danibanani.js@gmail.com>
2021-10-04 00:00:50 +0300

    Remove layout for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 
 import Canvas from "../components/three/canvas";
-import Layout from "../components/main/layout/layout";
 import SEO from "../components/main/seo/seo";
 
 const IndexPage: React.FC = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <div id="scene-container" style={{ height: "100vh", width: "100%" }}>
       <Canvas />
     </div>
-  </Layout>
+  </>
 );
 
 export default IndexPage;
