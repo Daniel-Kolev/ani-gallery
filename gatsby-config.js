83ada1b67fc8f74ce71d2d864566f846c800ef6e
Daniel Kolev <danibanani.js@gmail.com>
2021-10-03 23:29:02 +0300

    Setup Eslint+Prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,28 @@ module.exports = {
         icon: `src/favicon.png`,
       },
     },
+    {
+      resolve: "gatsby-plugin-prettier-eslint",
+      options: {
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            "**/*.{css,scss,less}",
+            "**/*.{json,json5}",
+            "**/*.{graphql}",
+            "**/*.{md,mdx}",
+            "**/*.{html}",
+            "**/*.{yaml,yml}",
+          ],
+        },
+        eslint: {
+          patterns: "**/*.{js,jsx,ts,tsx}",
+          customOptions: {
+            fix: true,
+            cache: true,
+          },
+        },
+      },
+    },
   ],
 };
