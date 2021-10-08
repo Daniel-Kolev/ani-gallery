1235bdc47a1d805da30ebec1fad098bde4edd8e3
Daniel Kolev <danibanani.js@gmail.com>
2021-10-08 18:09:07 +0300

    Remove unused plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
