9d9f271648e936e02481f102d6f2523136a3afd0
Daniel Kolev <danibanani.js@gmail.com>
2021-10-09 14:39:26 +0300

    Potential fix for ios device orientation

--- src/components/three/controls.tsx
+++ src/components/three/controls.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Mesh, Object3D, PerspectiveCamera, Vector3 } from "three";
+import React, { useEffect, useRef } from "react";
+import { Mesh, Object3D } from "three";
 import {
   PointerLockControls,
   DeviceOrientationControls,
@@ -14,9 +14,22 @@ interface ControlsProps {
 
 const Controls: React.FC<ControlsProps> = ({ floor }) => {
   const defaultCamera = useThree(({ camera }) => camera);
+  const deviceOrientationControls = useRef();
 
   useEffect(() => {
     defaultCamera.position.setY(Config.player.personHeight);
+
+    const hasCursor = matchMedia("(pointer:fine)").matches;
+    if (hasCursor) return;
+
+    const connectListeners = () => {
+      console.log(deviceOrientationControls.current);
+      deviceOrientationControls.current.connect();
+    };
+    document.addEventListener("touchend", connectListeners, false);
+    return () => {
+      document.removeEventListener("touchend", connectListeners, false);
+    };
   }, []);
 
   useMovement({
@@ -24,7 +37,14 @@ const Controls: React.FC<ControlsProps> = ({ floor }) => {
   });
 
   const hasCursor = matchMedia("(pointer:fine)").matches;
-  return hasCursor ? <PointerLockControls /> : <DeviceOrientationControls />;
+  return hasCursor ? (
+    <PointerLockControls />
+  ) : (
+    <DeviceOrientationControls
+      screenOrientation={90}
+      ref={deviceOrientationControls}
+    />
+  );
 };
 
 export default Controls;
