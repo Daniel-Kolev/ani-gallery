db4e4cd4f397e0f17a0eeeafcd8139b344bde82e
Daniel Kolev <danibanani.js@gmail.com>
2021-10-09 23:42:40 +0300

    Add canvas loader

--- src/components/three/canvas.tsx
+++ src/components/three/canvas.tsx
@@ -3,17 +3,21 @@ import { Canvas } from "@react-three/fiber";
 import floor from "utils/floors";
 import Controls from "components/three/controls";
 import Gallery from "components/three/gallery";
+import { Loader as CanvasLoader } from "@react-three/drei";
 
 const IndexPage: React.FC = () => {
   return (
-    <Canvas>
-      <Controls floor={floor} />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Suspense fallback={null}>
-        <Gallery />
-      </Suspense>
-    </Canvas>
+    <>
+      <Canvas>
+        <Controls floor={floor} />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Suspense fallback={null}>
+          <Gallery />
+        </Suspense>
+      </Canvas>
+      <CanvasLoader />
+    </>
   );
 };
 
